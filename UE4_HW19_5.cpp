5a0c73a005748dd44802f2438e3b2417f99d3763
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-29 00:51:46 +0300

    tmp cmt

--- UE4_HW19_5.cpp
+++ UE4_HW19_5.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 
-template<class T, class N>
-class A
+class Animal
 {
-private:
-	T value;
-	N another_value;
 public:
-	A() {}
-	A(T _value, N _another_value) : value(_value), another_value(_another_value) {}
-
-	void ShowValue()
+	Animal() {}
+	virtual void voice()
 	{
-		std::cout << value << std::endl;
+		std::cout << "animal says: " << std::endl;
 	}
 
-	void SetValue(T newValue)
+};
+
+class Dog : Animal
+{
+public:
+
+	void voice() override
 	{
-		value = newValue;
-	}
-	void ShowAnotherValue()
-	{
-		std::cout << another_value << std::endl;
-	}
-	void SetAnotherValue(N newAnotherValue)
-	{
-		another_value = newAnotherValue;
+		std::cout << "woof"<< std::endl;
 	}
 };
 
+class Cat : Animal
+{
+public:
+
+	void voice() override
+	{
+		std::cout << "myau" << std::endl;
+	}
+};
+
+class Cow : Animal
+{
+public:
+
+	void voice() override
+	{
+		std::cout << "mouu" << std::endl;
+	}
+};
+
+
 int main()
 {
-	A<double, int>* ptr = new A<double, int>(10.5, 6);
-	ptr->ShowValue();
-	ptr->ShowAnotherValue();
-	ptr->SetValue(4.5);
-	ptr->SetAnotherValue(11);
-	ptr->ShowValue();
-	ptr->ShowAnotherValue();
+	Animal none;
+	Dog* dog = new Dog();
+	Cat* cat = new Cat();
+	Cow* cow = new Cow();
+
+
 }
