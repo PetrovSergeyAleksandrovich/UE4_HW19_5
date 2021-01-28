fb9e1314ae5f5a7cb19eb820d592203d3a570af0
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-28 22:32:01 +0300

    tmp commit. template example with two values initialized

--- UE4_HW19_5.cpp
+++ UE4_HW19_5.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
 
+template<class T, class N>
+class A
+{
+private:
+	T value;
+	N another_value;
+public:
+	A() {}
+	A(T _value, N _another_value) : value(_value), another_value(_another_value) {}
+
+	void ShowValue()
+	{
+		std::cout << value << std::endl;
+	}
+
+	void SetValue(T newValue)
+	{
+		value = newValue;
+	}
+	void ShowAnotherValue()
+	{
+		std::cout << another_value << std::endl;
+	}
+	void SetAnotherValue(N newAnotherValue)
+	{
+		another_value = newAnotherValue;
+	}
+};
+
 int main()
 {
-
+	A<double, int>* ptr = new A<double, int>(10.5, 6);
+	ptr->ShowValue();
+	ptr->ShowAnotherValue();
+	ptr->SetValue(4.5);
+	ptr->SetAnotherValue(11);
+	ptr->ShowValue();
+	ptr->ShowAnotherValue();
 }
-
