0a9f4e453213421b7de2cf8eb31f85bf4ce9b99f
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-29 09:50:35 +0300

    tmp cmt. animals array exist filled with pointers.

--- UE4_HW19_5.cpp
+++ UE4_HW19_5.cpp
@@ -6,48 +6,53 @@ public:
 	Animal() {}
 	virtual void voice()
 	{
-		std::cout << "animal says: " << std::endl;
+		std::cout << "text" << std::endl;
 	}
 
 };
 
-class Dog : Animal
+class Dog : public Animal
 {
 public:
-
+	Dog() {}
 	void voice() override
 	{
 		std::cout << "woof"<< std::endl;
 	}
 };
 
-class Cat : Animal
+class Cat : public Animal
 {
 public:
-
+	Cat() {}
 	void voice() override
 	{
 		std::cout << "myau" << std::endl;
 	}
 };
 
-class Cow : Animal
+class Rat : public Animal
 {
 public:
-
+	Rat() {}
 	void voice() override
 	{
-		std::cout << "mouu" << std::endl;
+		std::cout << "shhh" << std::endl;
 	}
 };
 
-
 int main()
 {
-	Animal none;
-	Dog* dog = new Dog();
-	Cat* cat = new Cat();
-	Cow* cow = new Cow();
+	Animal* d = new Dog();
+	Animal* c = new Cat();
+	Animal* r = new Rat();
+
+	Animal* array[3] = { d, c, r };
+
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << array[0] + i << std::endl;
+	}
 
 
 }
